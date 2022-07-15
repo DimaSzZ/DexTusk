65290ca7d92485c68aceefca05b7c9aa58dcf614
Dmitry <komuk11107@mail.ru>
2022-07-15 16:24:12 +0300

    Task 13 commit

--- Task13Delegate/BankSystem.cs
+++ Task13Delegate/BankSystem.cs
@@ -2,33 +2,49 @@
 {
     internal class  BankSystem
     {
-        delegate double ConvertDelegate<in T, in B>(double money, T valueIn, B valueOut);
+        public delegate double ConvertDelegate<in T,in B>(double money, T valueIn, B valueOut);
 
-        private ConvertDelegate<> convertDelegate = Convertation;
+        public ConvertDelegate<object,object>? convertDelegateObject = Convertation;
 
-        public void AddCashAccount(Client client, Dictionary<Type, double>? listAccount)
+
+        public static void AddCashAccount(Client client, Dictionary<Type, double>? listAccount)
         {
-            if (client.Fio != null)
+            if (client.Fio == null) return;
+            if (Client.DictionaryCleints?[client.Fio] == null)
             {
-                Console.WriteLine("Выберите валюту: 1-Рубли 2-Леи 3-Гривна");
-                switch (Console.ReadLine())
+                if (listAccount != null)
                 {
-                    case "1":
-                        var a = Client.DictionaryCleints?[client.Fio];
-                        a?.Add(typeof(Rub),2500);
-                        break;
-                    case "2":
-                        var b = Client.DictionaryCleints?[client.Fio];
-                        b?.Add(typeof(Lei), 3000);
-                        break;
-                    case "3":
-                        var c = Client.DictionaryCleints?[client.Fio];
-                        c?.Add(typeof(Grivna), 4500);
-                        break;
+                    Client.DictionaryCleints?.Add(client.Fio, listAccount);
+                    Console.WriteLine("Такого клиента не было в базе данных, поэтому он был создан");
                 }
             }
+            Console.WriteLine("Выберите валюту: 1-Рубли 2-Леи 3-Гривна");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    var rubAc = Client.DictionaryCleints?[client.Fio];
+                    rubAc?.Add(typeof(Rub),2500);
+                    if (Client.DictionaryCleints != null)
+                        if (rubAc != null)
+                            Client.DictionaryCleints[client.Fio] = rubAc;
+                    break;
+                case "2":
+                    var leiAc = Client.DictionaryCleints?[client.Fio];
+                    leiAc?.Add(typeof(Rub), 2500);
+                    if (Client.DictionaryCleints != null)
+                        if (leiAc != null)
+                            Client.DictionaryCleints[client.Fio] = leiAc;
+                    break;
+                case "3":
+                    var grivAc = Client.DictionaryCleints?[client.Fio];
+                    grivAc?.Add(typeof(Rub), 2500);
+                    if (Client.DictionaryCleints != null)
+                        if (grivAc != null)
+                            Client.DictionaryCleints[client.Fio] = grivAc;
+                    break;
+            }
         }
-        public double Convertation<T, B>(double money, T valueIn, B valueOut)
+        public static double Convertation< T, B>(double money, T valueIn, B valueOut)
         {
             switch (valueIn)
             {
--- Task13Delegate/Program.cs
+++ Task13Delegate/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace Task13Delegate;
-
 internal class Program
 {
-    
     private static void Main()
     {
-
+        //BankSystem.AddCashAccount(new Client("Чорт"),new Dictionary<Type, double>());
+        var fio = new Client("Чорт").Fio;
+        if (fio != null)
+            Client.DictionaryCleints?.Add(fio, new Dictionary<Type, double>() {[typeof(Program)] = 2000});
+        var dictionaryCleint = Client.DictionaryCleints?["Чорт"];
+        if (dictionaryCleint == null) return;
+        foreach (var d in dictionaryCleint)
+        {
+            Console.WriteLine("Пиймал за сраку");
+        }
     }
 }
