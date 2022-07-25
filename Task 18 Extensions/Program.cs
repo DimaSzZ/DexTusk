30029f6040a3c1f3927e0ed77542e78f08049be2
Dmitry <komuk11107@mail.ru>
2022-07-25 11:46:28 +0300

    Taks 18

---
+++ Task 18 Extensions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_18_Extensions;
+class Program
+{
+    static void Main()
+    {
+        var subtractTheHours = 5;
+        var getInlyTime = subtractTheHours.HourNow(DateTime.Now);
+        Console.WriteLine($"{getInlyTime.Hours}:{getInlyTime.Minutes}:{getInlyTime.Seconds}");
+    }
+}
---
+++ Task 18 Extensions/TimeSpanOperationClass.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_18_Extensions
+{
+    public static class TimeSpanOperationClass
+    {
+        public static TimeSpan HourNow(this int time, DateTime ts)
+        {
+            return ts-ts.Date ;
+        }
+    }
+}
