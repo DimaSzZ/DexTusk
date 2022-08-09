72a9032760ec19de8767c78caf4f545d6226b2f0
DimaSzZ <90006552+DimaSzZ@users.noreply.github.com>
2022-08-09 19:42:14 +0300

    Delete Task14Events directory

--- Task14Events/Persona.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Task14Events
-{
-    internal class Persona
-    {
-        public string? Name { get; set; }
-        public string? LastName { get; set; }
-        public DateTime Birth { get; set; }
-    }
-}
