e0ebd58b7ac0922966dc3c8c43efd3483a77b446
DimaSzZ <90006552+DimaSzZ@users.noreply.github.com>
2022-08-09 19:39:47 +0300

    Delete Task 12 Generic Type, Generic Member(2) directory

--- Task 12 Generic Type, Generic Member(2)/Grivna.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Task_12_Generic_Type__Generic_Member_2_
-{
-    internal class Grivna 
-    {
-        public double Curs { get; set; } = 29.8;
-    }
-}
--- Task 12 Generic Type, Generic Member(2)/Lei.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Task_12_Generic_Type__Generic_Member_2_
-{
-    internal class Lei 
-    {
-        public double Curs { get; set; } = 19.22;
-    }
-}
