59068e802d70f991c33ba2f83829250621a956fe
DimaSzZ <90006552+DimaSzZ@users.noreply.github.com>
2022-08-09 19:42:07 +0300

    Delete Task13Delegate directory

--- Task13Delegate/Lei.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Task13Delegate
-{
-    internal class Lei
-    {
-        public double Curs { get; set; } = 19.22;
-    }
-}
--- Task13Delegate/Rub.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Task13Delegate
-{
-    internal class Rub 
-    {
-        public double Curs { get; set; } = 53.5;
-    }
-}
